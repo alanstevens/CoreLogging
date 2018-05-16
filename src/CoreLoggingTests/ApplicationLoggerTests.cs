eef3a2b900a1d060660cb52633eb7fa157d2e534
H. Alan Stevens <alanstevens@gmail.com>
2018-05-16 13:55:12 -0400

    Merge application logger tests and extension method tests. Closes #1

--- src/CoreLoggingTests/ApplicationLoggerTests.cs
+++ src/CoreLoggingTests/ApplicationLoggerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CoreLogging;
+using CoreLogging.Extensions;
 using NSubstitute;
 using Xunit;
 
@@ -9,17 +10,18 @@ namespace CoreLoggingTests
     {
         public ApplicationLoggerTests()
         {
-            _factory = Substitute.For<ICoreLoggerFactory>();
+            var factory = Substitute.For<ICoreLoggerFactory>();
             _logger = Substitute.For<ICoreLogger>();
-            _factory.CreateLogger(GetType()).Returns(_logger);
+            factory.CreateLogger(GetType()).Returns(_logger);
 
-            ApplicationLogger.Initialize(_factory);
+            ApplicationLogger.Initialize(factory);
         }
 
         readonly ICoreLogger _logger;
-        readonly ICoreLoggerFactory _factory;
         readonly string _message = "message";
         readonly Exception _exception = new Exception();
+
+        //------------------------------------------ApplicationLogger------------------------------------------//
 
         [Fact]
         public void should_call_logdebug()
@@ -103,6 +105,92 @@ namespace CoreLoggingTests
         {
             ApplicationLogger.LogCritical(this, _exception, _message);
             _logger.Received().LogCritical(_exception, _message, Arg.Any<object[]>());
+        }
+
+        //------------------------------------------ApplicationLogger Extensions------------------------------------------//
+
+        [Fact]
+        public void should_call_logcritical_extension()
+        {
+            this.LogCritical(_message);
+            _logger.Received().LogCritical(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logcritical_with_exception_extension()
+        {
+            this.LogCritical(_exception, _message);
+            _logger.Received().LogCritical(_exception, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logdebug_extension()
+        {
+            this.LogDebug(_message);
+            _logger.Received().LogDebug(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logdebug_with_exception_extension()
+        {
+            this.LogDebug(_exception, _message);
+            _logger.Received().LogDebug(_exception, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logerror_extension()
+        {
+            this.LogError(_message);
+            _logger.Received().LogError(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logerror_with_exception_extension()
+        {
+            this.LogError(_exception, _message);
+            _logger.Received().LogError(_exception, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_loginformation_extension()
+        {
+            this.LogInformation(_message);
+            _logger.Received().LogInformation(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_loginformation_with_exception_extension()
+        {
+            this.LogInformation(_exception, _message);
+            _logger.Received().LogInformation(_exception, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logtrace_extension()
+        {
+            this.LogTrace(_message);
+            _logger.Received().LogTrace(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logtrace_with_exception_extension()
+        {
+            this.LogTrace(_exception, _message);
+            _logger.Received().LogTrace(_exception, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logwarning_extension()
+        {
+            this.LogWarning(_message);
+            _logger.Received().LogWarning(null, _message, Arg.Any<object[]>());
+        }
+
+        [Fact]
+        public void should_call_logwarning_with_exception_extension()
+        {
+            this.LogWarning(_exception, _message);
+            _logger.Received().LogWarning(_exception, _message, Arg.Any<object[]>());
         }
     }
 }
